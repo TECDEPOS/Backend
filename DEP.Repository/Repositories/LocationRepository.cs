fe30dde33044d9a8977e29b1585827eba3641e1f
Sebastian Munch <seba2068@elev.tec.dk>
2023-08-17 12:45:48 +0200

    Location added

---
+++ DEP.Repository/Repositories/LocationRepository.cs
@@ -0,0 +1,49 @@
+﻿using DEP.Repository.Context;
+using DEP.Repository.Interfaces;
+using DEP.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEP.Repository.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly DatabaseContext context;
+        public LocationRepository(DatabaseContext context) { this.context = context; }
+
+        public async Task<Location> AddLocation(Location location)
+        {
+            context.Locations.Add(location);
+            await context.SaveChangesAsync();
+
+            return location;
+        }
+
+        public async Task<Location> DeleteLocation(int id)
+        {
+            var location = await context.Locations.FirstOrDefaultAsync(l => l.LocationId == id);
+
+            if (location != null)
+            {
+                context.Locations.Remove(location);
+                await context.SaveChangesAsync();
+            }
+
+            return location;
+        }
+
+        public async Task<List<Location>> GetLocations()
+        {
+            var locations = await context.Locations.ToListAsync();
+
+            return locations;
+        }
+
+        public async Task<Location> UpdateLocation(Location location)
+        {
+            context.Entry(location).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return location;
+        }
+    }
+}
---
+++ DEP.Service/Interfaces/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using DEP.Repository.Models;
+
+namespace DEP.Service.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<List<Location>> GetLocations();
+        Task<Location> DeleteLocation(int id);
+        Task<Location> AddLocation(Location location);
+        Task<Location> UpdateLocation(Location location);
+    }
+}
