bc191d06f3c1ca6011b37fd7fdbd3a078168f658
Snixert <swattlebagger@gmail.com>
2023-09-27 11:50:30 +0200

    adjust calls for new models and add a couple of new endpoints

--- DEP.Service/Interfaces/IPersonModuleService.cs
+++ DEP.Service/Interfaces/IPersonModuleService.cs
@@ -4,10 +4,12 @@ namespace DEP.Service.Interfaces
 {
     public interface IPersonModuleService
     {
-        Task<PersonModule> GetPersonModule(int personId, int moduleId, DateTime date);
+        Task<List<PersonModule>> GetAllPersonModules();
+        Task<PersonModule> GetPersonModule(int id);
         Task<List<PersonModule>> GetPersonModules(int personId, int moduleId);
+        Task<List<PersonModule>> GetPersonModulesByPerson(int personId);
         Task<PersonModule> AddPersonModule(PersonModule personModule);
         Task<PersonModule> UpdatePersonModule(PersonModule personModule);
-        Task<PersonModule> DeletePersonModule(int personId, int moduleId, DateTime date);
+        Task<PersonModule> DeletePersonModule(int id);
     }
 }
--- DEP.Service/Services/PersonModuleService.cs
+++ DEP.Service/Services/PersonModuleService.cs
@@ -9,24 +9,34 @@ namespace DEP.Service.Services
         private readonly IPersonModuleRepository repo;
         public PersonModuleService(IPersonModuleRepository repo) { this.repo = repo; }
 
+        public async Task<List<PersonModule>> GetAllPersonModules()
+        {
+            return await repo.GetAllPersonModules();
+        }
+
         public async Task<PersonModule> AddPersonModule(PersonModule personModule)
         {
             return await repo.AddPersonModule(personModule);
         }
 
-        public async Task<PersonModule> DeletePersonModule(int personId, int moduleId, DateTime date)
+        public async Task<PersonModule> DeletePersonModule(int id)
         {
-            return await repo.DeletePersonModule(personId, moduleId, date);
+            return await repo.DeletePersonModule(id);
         }
 
-        public async Task<PersonModule> GetPersonModule(int personId, int moduleId, DateTime date)
+        public async Task<PersonModule> GetPersonModule(int id)
         {
-            return await repo.GetPersonModule(personId,moduleId, date);
+            return await repo.GetPersonModule(id);
         }
 
         public async Task<List<PersonModule>> GetPersonModules(int personId, int moduleId)
         {
             return await repo.GetPersonModules(personId, moduleId);
+        }
+
+        public async Task<List<PersonModule>> GetPersonModulesByPerson(int personId)
+        {
+            return await repo.GetPersonModulesByPerson(personId);
         }
 
         public async Task<PersonModule> UpdatePersonModule(PersonModule personModule)
